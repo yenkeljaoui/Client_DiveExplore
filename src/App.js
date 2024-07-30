32a170f4a127b85656b4005284a903d1301bb521
Yoeli Barthel <yoelibarthel@MBP-de-Yoeli.lan>
2024-07-30 10:43:15 +0300

    Home.js and home.css with serveur but without database

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// src/App.js
 import React, { useState } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
@@ -21,10 +22,10 @@ function App() {
       });
       const data = await response.json();
       if (response.ok) {
-        setCurrentUser(username); // Set the current user to the username
+        setCurrentUser(username);
         return username;
       } else {
-        alert(data.message); // Show error message from server
+        alert(data.message);
         return null;
       }
     } catch (err) {
@@ -40,11 +41,11 @@ function App() {
       <Routes>
         <Route path="/signin" element={<SignIn signin={handleSignIn} />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/" element={currentUser ? <Home /> : <Navigate to="/signin" />} />
+        <Route path="/" element={currentUser ? <Home currentUser={currentUser} /> : <Navigate to="/signin" />} />
         <Route path="/about" element={currentUser ? <About /> : <Navigate to="/signin" />} />
         <Route path="/list-of-dives" element={currentUser ? <DiveSpots /> : <Navigate to="/signin" />} />
         <Route path="/dive-spot/:id" element={currentUser ? <DiveSpotDetails /> : <Navigate to="/signin" />} />
-        <Route path="/:username" element={currentUser ? <Home /> : <Navigate to="/signin" />} />
+        <Route path="/:username" element={currentUser ? <Home currentUser={currentUser} /> : <Navigate to="/signin" />} />
       </Routes>
     </div>
   );
